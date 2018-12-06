a686d8549bf87233db594e761cdf2a23736818d8
Ilene Cohen <ilene413@icloud.com>
2018-12-06 11:59:39 -0500

    working version

--- assets/javascript/app4 - last.js
+++ assets/javascript/app4 - last.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     let maxTimer = 3;
     let intervalId;
     let timeoutId;
-    let maxQuest = 2;
+    let maxQuest = 4;
     let questionCount = 0;
     let numCorrect = 0;
     let numIncorrect = 0;
@@ -49,72 +49,33 @@ $(document).ready(function () {
     let answerValue;
     let correctTrivia;
 
-    //start when player clicks on start
+    //start when player starts game
 
 
-    $("#start").click(function () {
+    $(".startBtn").on("click", "#start", function () {
         // initialize question asked array to "n" for NO
         //hide start button
         initQuestAsked();
-        $("button").hide();
-
-        // set up and play 
-
-        setupTrivia();
-
-    });
-
-    //play trivia game
-
-    $("#answersQ").on("click", ".answer-button", function () {
-
-        // determine which answer was selected & compare to correct answer
-
-        answerValue = $(this).attr("data-answer");
-        correctTrivia = triviaQuestions[questionNum].correctAnswer;
-        if (answerValue == correctTrivia) {
-            ansMessage = "Congratulations - correct guess";
-            numCorrect++;
-            updateScreen(ansMessage);
-        }
-        else {
-            ansMessage = "Incorrect - Correct Answer is  ";
-            numIncorrect++;
-            updateScreen(ansMessage);
-        }
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(setupTrivia, 3000);
+        $("#start").hide();
+    //    $("#triviagame").show();
+        // set up and play game
+        triviaGame();
 
     });
-
-    //check to see if want to play again
-
-    $("#gameOver").on("click", ".playagain-button", function () {
-        //  clear question asked array and initialize variables
-        initQuestAsked();
-        questionCount = 0;
-        numCorrect = 0;
-        numIncorrect = 0;
-        //clear screen for new game
-        //     $("#gameOver").hide();
-        $("#gameOver").empty();
-        //    $("#triviaGame").empty();
-        //   $("#triviaGame").show();
-        //
-        //   set up and play game again
-        setupTrivia();
-    });
-
     // this function plays the trivia game
 
-    function setupTrivia() {
+    function triviaGame() {
 
         //determine if game is over
+        console.log("question count = " + questionCount);
         if (questionCount < maxQuest) {
-// game not over
-// clear trivia game board
-            $("#triviaQ, #answersQ, #winOrLose, #correctImg").empty();
-            //        $("#triviaGame").empty();
+            //            endGame();
+            //
+            //        }
+            //        else {
+            // clear variables
+            $("#triviagame, #triviaQ, #answersQ, #winOrLose, #correctImg").empty();
+        //    $("#triviaGame").show();
 
 
             //get random question, mark question asked, keep count # of questions
@@ -123,21 +84,25 @@ $(document).ready(function () {
             //set up timer
 
             showTimer = maxTimer;
-            $("#timer").empty();
-            $("#timer").append("<p> Timer: " + showTimer + "</p>");
+            $("#timer").html("<p> Timer: " + showTimer + "</p>");
             clearInterval(intervalId);
             intervalId = setInterval(countDownTimer, 1000);
 
             questionNum = Math.floor(Math.random() * maxQuest);
-            if (questAsked[questionNum] == "n") {
+            console.log("question to be asked before checking" + questionNum);
+            if (questAsked[questionNum] === "n") {
                 questAsked[questionNum] = "y";
                 questionCount++;
+                console.log("question asked " + questionNum);
+                console.log("question count " + questionCount);
+
             }
             else {
-                setupTrivia();
+                triviaGame();
+                return;
             }
 
-            $("#triviaQ").empty();
+        //    $("#triviaQ").empty();
             $("#triviaQ").append("<p>" + triviaQuestions[questionNum].question);
 
             //set up answers
@@ -153,19 +118,77 @@ $(document).ready(function () {
                 $("#answersQ").append(j + ".  ").append(ansBtn).append("<br><br>");
 
             }
+            $("#answersQ").on("click", ".answer-button", function () {
 
+                // determine which answer was selected & compare to correct answer
+
+                answerValue = $(this).attr("data-answer");
+                correctTrivia = triviaQuestions[questionNum].correctAnswer;
+                if (answerValue == correctTrivia) {
+                    console.log("match");
+                    ansMessage = "Congratulations - correct guess";
+                    numCorrect++;
+                    updateScreen(ansMessage);
+                }
+                else {
+                    console.log("incorrect");
+                    ansMessage = "Incorrect - Correct Answer is  ";
+                    numIncorrect++;
+                    updateScreen(ansMessage);
+                }
+                console.log("question count before checking " + questionCount);
+                //    questionCount++;
+                console.log("question count after update & before checking " + questionCount);
+
+                // if (questionCount > maxQuest) {
+                //     console.log("in end game");
+                //    endgame();
+                //    return;
+                // }
+                // else {
+                console.log("game not over");
+                clearTimeout(timeoutId);
+                timeoutId = window.setTimeout(triviaGame, 3000);
+            //    return;
+                // }
+
+
+            });
         }
         else {
 
             //end of game - show totals 
-            endGame();
 
+            console.log("end of game " + questionCount);
+            endGame();
         }
+            //check to see if want to play again
+            console.log("back from end game function   ");
+            $("#gameOver").on("click", ".playagain-button", function () {
+                // play game again
+                console.log("in play again");
+                //            // clear question asked array and initialize variables
+                for (i = 0; i < maxQuest; i++) {
+                    questAsked.pop();
+                }
+        //        initQuestAsked();
+                questionCount = 0;
+                numCorrect = 0;
+                numIncorrect = 0;
+                //clear screen for new game
+             //   $("#gameOver").hide();
+                $("#triviaGame").empty();
+             //   $("#triviaGame").show();
+                //
+                //            // set up and play game again
+        //        triviaGame();
+            });
+        
     }
     // initializes question asked array
     function initQuestAsked() {
         for (var i = 0; i < maxQuest; i++) {
-            questAsked[i] = "n";
+            questAsked.push("n");
         }
     }
     // this function counts down the timer
@@ -177,10 +200,12 @@ $(document).ready(function () {
         if (showTimer <= 0) {
             ansMessage = "Time is up - correct answer is  ";
             numIncorrect++;
+            //    questionCount++;
             $("#timer").html("<p> Timer: " + showTimer + "</p>");
             updateScreen(ansMessage);
+            console.log("you lose");
             clearTimeout(timeoutId);
-            timeoutId = setTimeout(setupTrivia, 1000);
+            timeoutId = window.setTimeout(triviaGame, 3000);
 
         }
     }
@@ -190,6 +215,7 @@ $(document).ready(function () {
 
     function updateScreen(ansMessage) {
         ansMessage = ansMessage + triviaQuestions[questionNum].correctAnswer;
+        console.log(ansMessage);
         //display message
         $("#winOrLose, #correctImg").empty();
         $("#winOrLose").append(ansMessage);
@@ -207,27 +233,30 @@ $(document).ready(function () {
 
     //this function is displayed when game is over
     function endGame() {
-  //      $("#timer", "triviaQ", "#winOrLose", "#answersQ", "#correctImg").empty();
-        $("#timer").empty();
-        $("#triviaQ").empty();
-        $("#winOrLose").empty();
-        $("#answersQ").empty();
-        $("#correctImg").empty();
-
-
-
+        console.log("end game");
+     //   $("#triviagame").hide();
+        $("#triviagame").empty();
         $("#gameOver").empty();
+    //    $("#gameOver").show();
         let playAgainBtn = $("<button>");
         playAgainBtn.addClass("playagain-button");
         playAgainBtn.text("Play Again");
-        $("#gameOver").html("Game Over");
-        $("#gameOver").append("<br><br>");
-        $("#gameOver").append("Correct Guesses:  " + numCorrect);
-        $("#gameOver").append("<br><br>");
-        $("#gameOver").append("Incorrect Guesses:  " + numIncorrect);
-        $("#gameOver").append("<br><br>").append(playAgainBtn);
+        $("#gameOver").append("Game Over").append("<br><br>").append("Correct Guesses:  " + numCorrect).append("<br><br>").append("Incorrect Guesses:  " + numIncorrect).append("<br><br>").append(playAgainBtn);
 
+        // $("#gameOver").on("click", ".playagain-button", function () {
 
+        // $("#playAgain").click(function () {
+        //     console.log("in play again");
+        //            // initialize variables
+        //     initQuestAsked();
+        //     questionCount = 0;
+        //     numCorrect = 0;
+        //     numIncorrect = 0;
+        //    $("#gameover").hide();
+
+        //             // set up and play game
+        //    triviaGame();
+        // });
     }
 
 });
